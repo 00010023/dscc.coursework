ad28100f2e94bdb30726b50dd11e8d9d6617f500
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-10-25 17:29:32 +0500

    init scripts

---
+++ Database/init.sql
@@ -0,0 +1,6 @@
+INSERT INTO Authors (Name, Biography) VALUES (N'John Smith', N'John Smith is a renowned journalist and author, with over 20 years of experience in the field of investigative journalism.');
+INSERT INTO Authors (Name, Biography) VALUES (N'Jane Doe', N'Jane Doe is a bestselling novelist known for her thrilling suspense stories.');
+INSERT INTO Authors (Name, Biography) VALUES (N'Alice Johnson', N'Alice Johnson, a historian and academic, has written several books on ancient civilizations.');
+INSERT INTO Authors (Name, Biography) VALUES (N'Robert Brown', N'Robert Brown is a travel writer who has explored over 50 countries and documented his experiences.');
+INSERT INTO Authors (Name, Biography) VALUES (N'Emily Davis', N'Emily Davis is a poet and essayist, acclaimed for her profound insights into human nature.');
+
