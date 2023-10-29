94e4048f2502ef72ecc4a2dbcc86909392535daa
00010023 <54666588+orzklv@users.noreply.github.com>
2023-10-30 01:26:58 +0500

    docker db bootstrap

--- Database/init.sh
+++ Database/init.sh
@@ -1,11 +1,11 @@
-#run the setup script to create the DB and the schema in the DB
+#run the setup script to init the DB and the schema in the DB
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P AreY0uD0neW1thCW! -d master -i /create.sql
+    /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P AreY0uD0neW1thCW! -d master -i /init.sql
     if [ $? -eq 0 ]
     then
-        echo "create.sql completed"
+        echo "init.sql completed"
         break
     else
         echo "not ready yet..."
